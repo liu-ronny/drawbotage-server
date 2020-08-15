924873917a295da67898b5a91e7090b6fa6ac797
liu-ronny <54465266+liu-ronny@users.noreply.github.com>
2020-08-14 22:52:04 -0700

    Specify node engine in package.json

--- server.js
+++ server.js
@@ -1,19 +1,20 @@
 const app = require("express")();
+const cors = require("cors");
 const { v4: uuid } = require("uuid");
 const server = require("http").Server(app);
 const Connection = require("./connection/connection");
 
-// app.use(function (req, res, next) {
-//   res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
-//   res.header(
-//     "Access-Control-Allow-Headers",
-//     "Origin, X-Requested-With, Content-Type, Accept"
-//   );
-//   next();
-// });
-
 const connection = new Connection(server);
 
+if (app.get("env") === "development") {
+  app.use(cors());
+}
+
+app.set("port", process.env.PORT || 3000);
+
+/**
+ * Creates a unique room id and sends it to the client.
+ */
 app.post("/rooms", (req, res) => {
   let roomId = uuid();
 
@@ -24,11 +25,17 @@ app.post("/rooms", (req, res) => {
   res.send(roomId);
 });
 
+/**
+ * Checks whether the provided room id is valid.
+ */
 app.get("/rooms/valid/:id", (req, res) => {
   const roomId = req.params.id;
   res.send(connection.contains(roomId));
 });
 
+/**
+ * Checks whether the specified player is in the provided room id.
+ */
 app.get("/rooms/:roomID/players/:playerName", (req, res) => {
   const roomId = req.params.roomID;
   const playerName = req.params.playerName;
@@ -42,11 +49,11 @@ app.get("/rooms/:roomID/players/:playerName", (req, res) => {
   res.send(false);
 });
 
-// handle invalid routes
+/**
+ * Handles invalid routes.
+ */
 app.use(function (req, res) {
   res.sendStatus(404);
 });
 
-server.listen(8080, () => {
-  "Server listening at http://localhost:8080";
-});
+server.listen(app.get("port"));
