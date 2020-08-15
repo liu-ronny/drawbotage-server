046aa705740cc603ec6eb868a9d792a800ec76ed
liu-ronny <54465266+liu-ronny@users.noreply.github.com>
2020-08-14 21:47:02 -0700

    Remove CORS support in favor of proxying from the frontend server

--- server.js
+++ server.js
@@ -3,14 +3,14 @@ const { v4: uuid } = require("uuid");
 const server = require("http").Server(app);
 const Connection = require("./connection/connection");
 
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
+// app.use(function (req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept"
+//   );
+//   next();
+// });
 
 const connection = new Connection(server);
 
